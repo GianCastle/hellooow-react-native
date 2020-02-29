d8d6f30d0a4e83547cc9bceb1e803e634ed75781
Giancarlos Castillo <gc.c@outlook.com>
2020-02-29 00:08:07 -0400

    feat: finished hellooow sample

---
+++ App.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+
+import {images} from './images';
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const [imageWidth, setImageWidth] = useState(0);
+
+  const getIndex = i => (i < 0 ? 1 : i);
+
+  const image = images[getIndex(index)];
+  const onLayoutImageChange = e => setImageWidth(e.nativeEvent.layout.width);
+
+  const onChangeImage = e => {
+    const {locationX} = e.nativeEvent;
+    const imageCenter = imageWidth / 2;
+    const isXOnCenter = locationX < imageCenter;
+    const delta = isXOnCenter ? -1 : +1;
+
+    const nextImageIndex = (index + delta) % images.length;
+    setIndex(nextImageIndex);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.empty} />
+      <TouchableHighlight onPress={onChangeImage} style={styles.image}>
+        <View>
+          <Image
+            source={{uri: image.uri}}
+            style={styles.image}
+            onLayout={onLayoutImageChange}
+          />
+          <View styles={styles.labelWrapper}>
+            <Text style={styles.imageLabel}>{image.label}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+      <View style={styles.empty} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#7350ff',
+  },
+  image: {
+    flex: 3,
+    width: 320,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  imageLabel: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    color: 'white',
+    width: 320,
+    fontSize: 18,
+  },
+  empty: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('App', () => App);
+
+export default App;
---
+++ images.js
@@ -0,0 +1,27 @@
+export const images = [
+  {
+    uri:
+      'https://images.unsplash.com/photo-1519996791471-6d89a2546faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjU2MDh9&auto=format&fit=crop&w=2250&q=80',
+    label: 'Happy Independence',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1559054554-dae1268e32cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2252&q=80',
+    label: 'Juan Pablo Duarte - Padre de la Patria',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1535827841776-24afc1e255ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
+    label: 'Hard Rock Hotel & Casino 8A, Punta Cana, Dominican Republic',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1551126819-d5a8a759b378?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=881&q=80',
+    label: 'Bavaro Beach, Punta Cana, Dominican Republic',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1521818859213-d0f03291327a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+    label: 'Sosúa, Dominican Republic - Dia internacional de la mujer',
+  },
+];
